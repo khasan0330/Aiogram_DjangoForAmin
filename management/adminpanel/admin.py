95b274a075b9169166ad2bd4a2fea216f660a5e9
khasan0330 <93327758+khasan0330@users.noreply.github.com>
2023-06-01 11:05:41 +0500

    commit

--- management/adminpanel/admin.py
+++ management/adminpanel/admin.py
@@ -11,7 +11,6 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'product_category', 'product_price', 'get_photo')
 
     def get_photo(self, obj):
-        print(obj.product_image.url)
         return mark_safe(f'<img src="{obj.product_image.url}" width="75">')
 
     get_photo.short_description = 'Миниатюра'
